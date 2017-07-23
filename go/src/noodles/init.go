c0547446529de53e28033f76b9beb0451650f674
Joshua Strobl <joshua@stroblindustries.com>
2017-07-23 22:27:54 +0300

    Fix noodles.yml existing check.

--- go/src/noodles/init.go
+++ go/src/noodles/init.go
@@ -8,7 +8,7 @@ import (
 )
 
 func initNoodles(cmd *cobra.Command, args []string) {
-	if configInfo, statErr := os.Stat("noodles.yml"); os.IsExist(statErr) { // Check if noodles.yml already exists
+	if configInfo, statErr := os.Stat("noodles.yml"); statErr == nil { // Check if noodles.yml already exists
 		if configInfo.Size() != 0 { // If the size of the file is greater than 0, meaning there is potentially content
 			fmt.Println("noodles.yml already exists and appears to have content. Exiting.")
 			return
