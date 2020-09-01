0efa41d116baf8745cb07a2a8804a0ace71337bf
Joshua Strobl <joshua@streambits.io>
2020-09-01 21:20:23 +0300

    Move from BurntSushi/toml to pelletier/go-toml. Update deps. Fix variable name.

--- go/src/noodles/config.go
+++ go/src/noodles/config.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"bytes"
 	"errors"
-	"github.com/BurntSushi/toml"
+	"github.com/pelletier/go-toml"
 	"github.com/stroblindustries/coreutils"
+	"io/ioutil"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -28,6 +30,7 @@ type NoodlesDistributionConfig struct {
 
 var SupportedTarCompressors []string // SupportedTarCompressions are various compressions we officially support
 var noodles NoodlesConfig            // Our Noodles Config
+var desiredConfigPath string
 
 func init() {
 	SupportedTarCompressors = []string{"bzip2", "gzip", "lzma", "xz", "zstd"}
@@ -35,78 +38,82 @@ func init() {
 
 // ReadConfig will read any local noodles.toml that exists and returns an error or NoodlesConfig
 func ReadConfig(configPath string) (conf NoodlesConfig, readConfigErr error) {
-	if _, convErr := toml.DecodeFile(configPath, &conf); convErr == nil { // Decode our config
-		if conf.Distribution == nil { // Distribution not set
-			conf.Distribution = &NoodlesDistributionConfig{
-				TarCompressors: []string{"zstd"},
-			}
+	desiredConfigPath = configPath
+	var configContent []byte
+
+	if configContent, readConfigErr = ioutil.ReadFile(configPath); readConfigErr != nil { // If we failed to read the config
+		if os.IsNotExist(readConfigErr) { // If the file does not exist
+			readConfigErr = errors.New("noodles.toml does not exist in this directory")
+		} else if os.IsPermission(readConfigErr) { // If we don't have the necessary permissions to read this file
+			readConfigErr = errors.New("noodles.toml is not readable in the provided path")
 		}
 
-		compressors := conf.Distribution.TarCompressors
+		return
+	}
 
-		if len(compressors) == 0 {
-			compressors = []string{"zstd"} // Default to zstd
-			conf.Distribution.TarCompressors = compressors
+	if readConfigErr = toml.Unmarshal(configContent, &conf); readConfigErr != nil { // If we failed to unmarshal our config content bytes into the conf NoodlesConfig
+		return
+	}
+
+	if conf.Distribution == nil { // Distribution not set
+		conf.Distribution = &NoodlesDistributionConfig{
+			TarCompressors: []string{"zstd"},
+		}
+	}
+
+	compressors := conf.Distribution.TarCompressors
+
+	if len(compressors) == 0 {
+		compressors = []string{"zstd"} // Default to zstd
+		conf.Distribution.TarCompressors = compressors
+	}
+
+	for _, compressor := range compressors {
+		if !ListContains(SupportedTarCompressors, compressor) { // If the provided compressor isn't supported
+			readConfigErr = errors.New("Must use a supported compressor: " + strings.Join(SupportedTarCompressors, ","))
+			return
+		}
+	}
+
+	for name, project := range conf.Projects { // For each noodles project
+		if project.ConsolidateChildDirs && (project.SimpleName == "") { // No SimpleName defined, and it'll be required during consolidation
+			project.SimpleName = name
 		}
 
-		for _, compressor := range compressors {
-			if !ListContains(SupportedTarCompressors, compressor) { // If the provided compressor isn't supported
-				readConfigErr = errors.New("Must use a supported compressor: " + strings.Join(SupportedTarCompressors, ","))
-				return
-			}
+		project.SourceDir = filepath.Dir(project.Source)
+
+		if project.SourceDir != "" { // If SourceDir has content
+			project.SourceDir = project.SourceDir + "/" // Add trailing /
 		}
 
-		for name, project := range conf.Projects { // For each noodles project
-			if project.ConsolidateChildDirs && (project.SimpleName == "") { // No SimpleName defined, and it'll be required during consolidation
-				project.SimpleName = name
-			}
+		if project.Type == "go" { // If this is a Go project
+			if len(project.ExcludeItems) == 0 { // No items
+				project.ExcludeItems = []string{"pkg/", "_test.go"} // Add pkg folder and _test.go files
+			} else { // Has items
+				if !ListContains(project.ExcludeItems, "pkg") {
+					project.ExcludeItems = append(project.ExcludeItems, "pkg")
+				}
 
-			project.SourceDir = filepath.Dir(project.Source)
-
-			if project.SourceDir != "" { // If SourceDir has content
-				project.SourceDir = project.SourceDir + "/" // Add trailing /
-			}
-
-			if project.Type == "go" { // If this is a Go project
-				if len(project.ExcludeItems) == 0 { // No items
-					project.ExcludeItems = []string{"pkg/", "_test.go"} // Add pkg folder and _test.go files
-				} else { // Has items
-					if !ListContains(project.ExcludeItems, "pkg") {
-						project.ExcludeItems = append(project.ExcludeItems, "pkg")
-					}
-
-					if !ListContains(project.ExcludeItems, "_test.go") {
-						project.ExcludeItems = append(project.ExcludeItems, "_test.go")
-					}
+				if !ListContains(project.ExcludeItems, "_test.go") {
+					project.ExcludeItems = append(project.ExcludeItems, "_test.go")
 				}
 			}
+		}
 
-			conf.Projects[name] = project
-		}
-	} else { // If there was an error decoding'
-		if strings.Contains(convErr.Error(), "no such file or directory") {
-			readConfigErr = errors.New("noodles.toml does not exist in this directory")
-		} else { // If this is some sort of other error, sanitize it and return a new convErr
-			sanitizedErrMessage := strings.Replace(convErr.Error(), "unmarshal", "convert", -1) // Change "unmarshal" to a human language
-			sanitizedErrMessage = strings.Replace(sanitizedErrMessage, "!!", "", -1)
-			sanitizedErrMessage = strings.Replace(sanitizedErrMessage, "`", "", -1) // Remove any ` wrapping types
-			readConfigErr = errors.New(sanitizedErrMessage)                         // Create a sanitized error
-		}
+		conf.Projects[name] = project
 	}
 
 	return
 }
 
 // SaveConfig will save the NoodlesConfig to noodles.toml
-func SaveConfig() error {
-	var saveErr error
+func SaveConfig() (saveErr error) {
 	var buffer bytes.Buffer
 	writer := bufio.NewWriter(&buffer)
-	encoder := toml.NewEncoder(writer) // Create a new toml encoder
-	encoder.Indent = "\t"              // Use a tab because we're opinionated
+	encoder := toml.NewEncoder(writer).Indentation("\t") // Create a new toml encoder and set its indentation to \t
 
 	if saveErr = encoder.Encode(noodles); saveErr == nil { // Encode our noodles struct into a buffer
-		saveErr = coreutils.WriteOrUpdateFile("noodles.toml", buffer.Bytes(), coreutils.NonGlobalFileMode) // Write the noodles.toml as non-global
+		saveErr = coreutils.WriteOrUpdateFile(desiredConfigPath, buffer.Bytes(), coreutils.NonGlobalFileMode) // Write the noodles.toml as non-global
 	}
 
 	return saveErr
